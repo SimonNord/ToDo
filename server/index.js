ee178ddce6ff4abda1357eed5da4041c4e7bb611
Simon <simon.nordstrom@chasacademy.se>
2020-08-25 22:33:44 +0200

    Create components for  - list  - listItem  - NewToDo input
     - add state in app component
     - add useEffect to get api data on component mount and when newPost updates.
     - add handlers for:
       - onChange, onSubmit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("This is the landing page");
